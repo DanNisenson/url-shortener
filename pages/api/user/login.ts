f6aea37dc2d216aff45cc73854abe7651f222f19
DanNisenson <danpablo1@gmail.com>
2023-03-08 17:29:10 +0100

    🔨 Start login controller. Move findUserByEmail to userQueries

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -5,12 +5,19 @@
 // - tokenize userId
 // - res.({ token })
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { Response, ErrorResponse } from '@/server.types'
 import jwt from 'jsonwebtoken'
 import * as argon2 from 'argon2'
+import validateAuthInfo from '@/server/validation/validateAuthInfo'
 
 export default async function signUp(
   req: NextApiRequest,
   res: NextApiResponse<Response | ErrorResponse>,
 ) {
+  const { email, password } = req.body
 
+  const isInfoValid = validateAuthInfo(email, password)
+  if (isInfoValid !== 1) return res.status(400).json(isInfoValid)
+
+  
 }
